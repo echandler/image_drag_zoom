1c6e25a097157091f4b787234881450a6ae46386
echandler <chandleric@hotmail.com>
2019-04-07 02:46:07 -0700

    Refactored into a class and tested. Is working well.

--- zoom.js
+++ zoom.js
@@ -1,102 +1,142 @@
-//https://jsbin.com/sotelitiqe/edit?html,css,js,output#J:L15
+//https://jsbin.com/cazuzad/3/edit?html,css,js,output
 
-let worldExt = {
-    x: 0,
-    X: 0,
-    y: 0,
-    Y: 0,
-};
+class ImgZoom {
+    constructor(cont, img) {
+        this.contData = {
+            container: cont,
+        };
 
-let vExtent = {
-    x: 0,
-    X: 660,
-    y: 0,
-    Y: 120,
-    scale: 1,
-};
+        this.imgData = {
+            img: img,
+            scale: 1,
+        };
 
-let img = document.getElementById('img');
-let cont = document.getElementById('cont');
-img.style.transformOrigin = 'top left';
+        this.resetImgData();
+        this.setContainerData();
 
-function chngExt(scale, xPercent, yPercent) {
-    let distX = vExtent.X - vExtent.x;
-    let distY = vExtent.Y - vExtent.y;
+        img.style.transformOrigin = 'top left';
 
-    let _x = distX * xPercent * (1 - scale);
-    let _y = distY * yPercent * (1 - scale);
+        img.addEventListener('mousewheel', this);
+        img.addEventListener('mousedown', this);
+    }
 
-    vExtent.scale *= scale;
+    handleEvent(evt) {
+        let type = evt.type;
 
-    vExtent.x += _x;
-    vExtent.X = vExtent.x + distX * scale;
-    vExtent.y += _y;
-    vExtent.Y = vExtent.y + distY * scale;
+        if (type === 'mousewheel') {
+            this.mousewheel(evt);
+        } else if (type === 'mousedown') {
+            this.mousedown(evt);
+        } else if (type === 'mouseup') {
+            this.mouseup(evt);
+        } else if (type === 'mousemove') {
+            this.drag(evt);
+        }
+    }
 
-    console.log('sdf', _x, _y, vExtent.x, vExtent.X);
-    img.style.transform = `translate(${vExtent.x}px, ${vExtent.y}px)
-                           scale(${vExtent.scale})`;
-}
+    resetImgData() {
+        let img = this.imgData.img;
 
-function pointOnImg(x, y) {
-    let _x = x - vExtent.x;
-    let _y = y - vExtent.y;
-    console.log('hi', _x, [vExtent.x, vExtent.X], x);
-    if (x > vExtent.x && x < vExtent.X) {
-        _x = _x / (vExtent.X - vExtent.x);
-        _y = _y / (vExtent.Y - vExtent.y);
-        console.log('hi3', _x, vExtent.x, x);
+        img.style.transform = '';
+
+        this.imgData.x = 0;
+        this.imgData.X = img.offsetWidth;
+        this.imgData.y = 0;
+        this.imgData.Y = img.offsetHeight;
+    }
+
+    setContainerData() {
+        let cont = this.contData.container;
+        let bbox = cont.getBoundingClientRect();
+
+        this.contData.x = bbox.x;
+        this.contData.X = bbox.x + bbox.width;
+        this.contData.y = bbox.y;
+        this.contData.Y = bbox.y + bbox.height;
+    }
+
+    zoom(scale, xPercent, yPercent) {
+        let imgData = this.imgData;
+        let img = imgData.img;
+        let distX = imgData.X - imgData.x;
+        let distY = imgData.Y - imgData.y;
+
+        let _x = distX * xPercent * (1 - scale);
+        let _y = distY * yPercent * (1 - scale);
+
+        imgData.scale *= scale;
+
+        imgData.x += _x;
+        imgData.X = imgData.x + distX * scale;
+        imgData.y += _y;
+        imgData.Y = imgData.y + distY * scale;
+
+        img.style.transition = '150ms transform ease';
+
+        this.updateImgTransform();
+    }
+
+    mousedown(evt) {
+        this.pnt = {x: evt.x, y: evt.y};
+
+        evt.preventDefault();
+
+        this.imgData.img.style.transition = '';
+
+        document.body.addEventListener('mousemove', this);
+        document.body.addEventListener('mouseup', this);
+    }
+
+    mouseup(evt) {
+        document.body.removeEventListener('mousemove', this);
+        document.body.removeEventListener('mouseup', this);
+    }
+
+    drag(evt) {
+        let imgData = this.imgData;
+        let difX = evt.x - this.pnt.x;
+        let difY = evt.y - this.pnt.y;
+        let img = imgData.img;
+
+        this.pnt.x = evt.x;
+        this.pnt.y = evt.y;
+
+        imgData.x += difX;
+        imgData.X += difX;
+        imgData.y += difY;
+        imgData.Y += difY;
+
+        this.updateImgTransform();
+    }
+
+    updateImgTransform() {
+        let imgData = this.imgData;
+        imgData.img.style.transform = `translate(${imgData.x}px, ${imgData.y}px)
+                                       scale(${imgData.scale})`;
+    }
+
+    pointPercentOnImg(x, y) {
+        let contData = this.contData;
+        let imgData = this.imgData;
+        let tx = imgData.x + contData.x;
+        let ty = imgData.y + contData.y;
+
+        let _x = x - tx;
+        let _y = y - ty;
+
+        _x = _x / (imgData.X - imgData.x);
+        _y = _y / (imgData.Y - imgData.y);
+
         return {x: _x, y: _y};
     }
-    return {x: NaN, y: NaN};
-}
 
-cont.addEventListener('mousedown', mousedown);
-
-function mousedown(evt) {
-    let pnt = {x: evt.x, y: evt.y};
-    evt.preventDefault();
-
-    if (evt.x > vExtent.x && evt.x < vExtent.X) {
-        cont.removeEventListener('mousedown', mousedown);
-        document.body.addEventListener('mousemove', mousemove);
-        document.body.addEventListener('mouseup', mouseup);
-    }
-
-    function mouseup(evt) {
-        cont.addEventListener('mousedown', mousedown);
-        document.body.removeEventListener('mousemove', mousemove);
-        document.body.removeEventListener('mouseup', mouseup);
-    }
-
-    function mousemove(evt) {
-        let difX = evt.x - pnt.x;
-        let difY = evt.y - pnt.y;
-        pnt.x = evt.x;
-        pnt.y = evt.y;
-
-        vExtent.x += difX;
-        vExtent.X += difX;
-        vExtent.y += difY;
-        vExtent.Y += difY;
-        img.style.transform = `translate(${vExtent.x}px, ${vExtent.y}px)
-                           scale(${vExtent.scale})`;
-    }
-}
-
-cont.addEventListener('mousewheel', function(evt) {
-    console.log(evt);
-
-    let pnt = pointOnImg(evt.x - 9, evt.y - 9);
-
-    if (!isNaN(pnt.x)) {
+    mousewheel(evt) {
         let dir = evt.wheelDelta > 0 ? 2 : 0.5;
-        chngExt(dir, pnt.x, pnt.y);
+        let pnt = this.pointPercentOnImg(evt.x, evt.y);
+        this.zoom(dir, pnt.x, pnt.y);
     }
-});
+}
 
-//let pnt = pointOnImg(100,0);
-//console.log(pnt)
-//chngExt(0.5, pnt.x, 0);
-//pnt = pointOnImg(10,0);
-//chngExt(0.5, pnt.x, 0);
+
+
+
