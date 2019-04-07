09aaeba562533b9bb8a1603327688af17d5b0f46
echandler <chandleric@hotmail.com>
2019-04-06 19:04:05 -0700

    Zooming and dragging functionality works.

--- zoom.js
+++ zoom.js
@@ -1,7 +1,11 @@
-
-
 //https://jsbin.com/sotelitiqe/edit?html,css,js,output#J:L15
 
+let worldExt = {
+    x: 0,
+    X: 0,
+    y: 0,
+    Y: 0,
+};
 
 let vExtent = {
     x: 0,
@@ -17,27 +21,32 @@ img.style.transformOrigin = 'top left';
 
 function chngExt(scale, xPercent, yPercent) {
     let distX = vExtent.X - vExtent.x;
+    let distY = vExtent.Y - vExtent.y;
+
     let _x = distX * xPercent * (1 - scale);
-    let _y = (vExtent.Y - vExtent.y) * yPercent * scale;
+    let _y = distY * yPercent * (1 - scale);
 
     vExtent.scale *= scale;
 
     vExtent.x += _x;
     vExtent.X = vExtent.x + distX * scale;
+    vExtent.y += _y;
+    vExtent.Y = vExtent.y + distY * scale;
 
     console.log('sdf', _x, _y, vExtent.x, vExtent.X);
-    img.style.transform = `translate(${vExtent.x}px, ${_y}px)
-                                   scale(${vExtent.scale})`;
+    img.style.transform = `translate(${vExtent.x}px, ${vExtent.y}px)
+                           scale(${vExtent.scale})`;
 }
 
 function pointOnImg(x, y) {
     let _x = x - vExtent.x;
-    let _y = 0;
+    let _y = y - vExtent.y;
     console.log('hi', _x, [vExtent.x, vExtent.X], x);
     if (x > vExtent.x && x < vExtent.X) {
         _x = _x / (vExtent.X - vExtent.x);
+        _y = _y / (vExtent.Y - vExtent.y);
         console.log('hi3', _x, vExtent.x, x);
-        return {x: _x, y: 0};
+        return {x: _x, y: _y};
     }
     return {x: NaN, y: NaN};
 }
@@ -62,16 +71,32 @@ function mousedown(evt) {
 
     function mousemove(evt) {
         let difX = evt.x - pnt.x;
+        let difY = evt.y - pnt.y;
         pnt.x = evt.x;
+        pnt.y = evt.y;
+
         vExtent.x += difX;
         vExtent.X += difX;
-        img.style.transform = `translate(${vExtent.x}px, 0px)`;
+        vExtent.y += difY;
+        vExtent.Y += difY;
+        img.style.transform = `translate(${vExtent.x}px, ${vExtent.y}px)
+                           scale(${vExtent.scale})`;
     }
 }
 
 cont.addEventListener('mousewheel', function(evt) {
-    let pnt = pointOnImg(evt.x - 9, 0);
+    console.log(evt);
+
+    let pnt = pointOnImg(evt.x - 9, evt.y - 9);
+
     if (!isNaN(pnt.x)) {
-        chngExt(2, pnt.x, 0);
+        let dir = evt.wheelDelta > 0 ? 2 : 0.5;
+        chngExt(dir, pnt.x, pnt.y);
     }
 });
+
+//let pnt = pointOnImg(100,0);
+//console.log(pnt)
+//chngExt(0.5, pnt.x, 0);
+//pnt = pointOnImg(10,0);
+//chngExt(0.5, pnt.x, 0);
